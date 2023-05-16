08ea32aab7efc9594047b4921063c9d44a4f430a
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-05-16 17:31:20 +0900

    Create 자바 아스키코드.java

---
+++ 자바 아스키코드.java
@@ -0,0 +1,21 @@
+package sdw;
+import java.util.Scanner;
+
+import sun.jvm.hotspot.gc.parallel.PSYoungGen;
+public class Yjs {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+		
+//3번 문제
+char a = 'S';
+char b = 'U';
+char c = 'N';
+char d = 'G';
+char e = 'I';
+char f = 'L';
+
+int sum = a+b+c+d+e+f;
+System.out.println(sum);
+
+
+
