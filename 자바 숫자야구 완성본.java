7993f91ea4974f698d47b96ddfbf54fdb0624d48
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-05-24 17:05:21 +0900

    Create 자바 숫자야구 완성본.java

---
+++ 자바 숫자야구 완성본.java
@@ -0,0 +1,53 @@
+package adw;
+import java.util.Random;
+import java.util.Scanner;
+public class Hjs {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Random  random = new Random();
+		
+		int a = random.nextInt(9)+1;
+		int b = random.nextInt(9)+1;
+		int c = random.nextInt(9)+1;
+	
+		while(a == b || b == c || a == c) {
+			a = random.nextInt(9)+1;
+		    b = random.nextInt(9)+1;
+			c = random.nextInt(9)+1;
+		}
+		while(true) {
+			System.out.print("숫자 3개 입력:");
+			int jyt = scanner.nextInt();
+			
+			if(jyt>=999) {
+				System.out.println("다시 나가");
+			}else {
+				int t = jyt % 10;
+				jyt = jyt / 10;
+				int y = jyt % 10;
+				jyt = jyt / 10;
+				int j = jyt % 10;
+				jyt = jyt / 10;
+				int strike = 0;
+				int ball = 0;
+				
+				if(a == j) strike ++;
+				else if(a==y||a==t)ball++;
+				if(b == y) strike ++;
+				else if(b==j||b==t)ball++;
+				if(c == t) strike ++;
+				else if(c==j||c==y)ball++;
+				System.out.println("스트라이크"+strike);
+				System.out.println("볼"+ball);
+				if(strike==3) {
+					break;
+				}
+				
+			}
+			
+			
+		}
+	}
+	
+}
