9c22e6219c3849434280c970dd685f6bebdd4cb4
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-06-08 14:18:48 +0900

    Create 정보.py

---
+++ 정보.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a >= 90 and a < 101:
+  print("A")
+elif a >= 70 and a < 90:
+  print("B")
+elif a >= 40 and a <70:
+  print("C")
+elif a >=0 and a < 40:
+  print("D")
+else:
+  print("다시")  
