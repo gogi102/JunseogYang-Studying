584e38d499d8746feb60f8845116505d96392a4b
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-06-08 14:19:17 +0900

    Create 정보 저장.py

---
+++ 정보 저장.py
@@ -0,0 +1,13 @@
+a = input()
+if a == "수":
+  print("A")
+elif a == "우":
+  print("B")
+elif a == "미":
+  print("C")
+elif a == "양":
+  print("D")
+elif a == "가":
+  print("E")   
+else:
+  print("다시")       
