269ed3b061ab6b406a5d29631b505de815435b16
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-06-21 17:21:32 +0900

    Create 계산기.java

---
+++ 계산기.java
@@ -0,0 +1,61 @@
+package wlqrkrhtlvek;
+import java.util.Scanner;
+public class Tlqkf {
+
+		  public static void main(String[] args) {
+		  Scanner sc = new Scanner(System.in);
+	        int num1, num2;
+
+	        System.out.print("num1?");
+	        num1 = sc.nextInt();
+	        System.out.print("num2?");
+	        num2 = sc.nextInt();
+	       
+	        // 사칙연산을 선택하게끔
+	        System.out.print("+(1) -(2) *(3) /(4)");
+	        int op = sc.nextInt();        
+	        double result = 0;
+	       
+	        // 계산 결과를 출력
+	        switch(op) {
+	        case 1:
+	            result = add(num1, num2);
+	            break;
+	        case 2:
+	            result = 빼기(num1,num2);
+	            break;
+	        case 3:
+	            result = 곱하기(num1,num2);
+	            break;
+	        case 4:
+	            // 더블 = 인트/인트
+	            result = 나누기(num1,num2);
+	            break;
+	        default:
+	            System.out.print("응 다시해~");        
+	        }
+	        System.out.println("결과:"+result);
+	    }
+	    
+	
+static int add(int a, int b) {
+	int result;
+	result = a+b;
+	return result;
+}		
+static int 빼기(int a, int b) {
+	int result;
+	result = a-b;
+	return result;
+}		
+static int 곱하기(int a, int b) {
+	int result;
+	result = a*b;
+	return result;
+}		
+static int 나누기(int a, int b) {
+	int result;
+	result = a/b;
+	return result;
+}		
+}
