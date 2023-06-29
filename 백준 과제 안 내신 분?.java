f43196c23476c29737221de1dcf24227ac4c3ca7
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-06-29 10:19:56 +0900

    Create 백준 과제 안 내신 분?.java

---
+++ 백준 과제 안 내신 분?.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int [] check = new int [31];
+
+        for(int i = 1; i <= 30; i++){
+            check[i] = 0;
+        }
+
+        for(int i = 0; i < 28; i++) {
+            int n = sc.nextInt();
+            check[n] = 1;
+        }
+
+        for(int i = 1; i <= 30; i++){
+            if(check[i] != 1){
+                System.out.println(i);
+            }
+        }
+    }
+}
