3027c064f3de06dd5bd8a1a2871af5a55ebb8b88
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-07-12 17:49:28 +0900

    Create 자바 시험1.java

---
+++ 자바 시험1.java
@@ -0,0 +1,11 @@
+package test;
+public class Test {
+	public static void main(String[] args) {
+		int num = 0;
+		for(int i = 1; i<=99; i+=2) {
+			num = num + i;
+		}
+		System.out.println(num);
+	}
+
+}
