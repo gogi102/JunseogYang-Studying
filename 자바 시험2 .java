3c4699797f4f85b4f456ef6245da90636c6daaa6
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-07-12 17:49:54 +0900

    Create 자바 시험2 .java

---
+++ 자바 시험2 .java
@@ -0,0 +1,13 @@
+package test;
+public class Test2 {
+	public static void main(String[] args) {
+		char s = 'S';
+		char u = 'U';
+		char n = 'N';
+		char g = 'G';
+		char i = 'I';
+		char l = 'L';
+		System.out.println(s+u+n+g+i+l);
+	}
+
+}
