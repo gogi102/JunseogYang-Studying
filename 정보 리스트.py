d46ea1ac6cb9c256896fa5435992cf9949950204
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-08-16 09:55:56 +0900

    Create 정보 리스트.py

---
+++ 정보 리스트.py
@@ -0,0 +1,24 @@
+a = []
+
+for i in range(1,11):
+  a.append(i)
+
+for i in range(1,4):
+  a.pop()
+
+print(a) 
+
+a.append("f")
+a.append("k")
+a.append("d")
+a.append("w")
+a.append("z")
+a.append("f")
+a.append("g")
+
+print(a)
+
+for i in range(1,6):
+  a.pop()
+print(a) 
+
