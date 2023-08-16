45a7f1c5509181ebd72b753c75dac4b1b1192a62
Yang jun seog <127902276+gogi102@users.noreply.github.com>
2023-08-16 09:54:58 +0900

    Create 정보2312.py

---
+++ 정보2312.py
@@ -0,0 +1,8 @@
+for i in range(2, 6):
+  for j in range(1, 10):
+    print("{} X {} = {}".format(i,j,i*j))
+  print("----------------------")
+#Queue = 선입선출 Enqueue(넣기) Dequeue(빼기)
+#Stack = 후입선출, 넣는 연산(push), 빼는 연산(pop)
+#가장 윗 부분은 Top, 가장 아랫 부분은 bottom
+#앞 부분 Front 뒷 부분 Rear
